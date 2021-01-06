66a43f03f2d92ba6d24bb7da5a6f223491277198
Taylor Treece <tayloratreece@gmail.com>
2021-01-06 16:40:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -53,7 +53,7 @@ def monopoly_with_fourth_tier
 			:pieces => 4,
 			:names => {
 				:reading_railroad => {:mortgage_value => '$100'},
-				:pennsylvania_railroad => {},
+				:pennsylvania_railroad => {:morgage_value},
 				:b_and_o_railroad => {},
 				:shortline => {},
 			},
